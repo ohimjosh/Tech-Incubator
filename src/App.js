06fe223c3019ef85a83b403d1c761e84b3876001
Josh <joshmiranda.exe@gmail.com>
2022-07-12 19:10:20 -0400

    login and signup handle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import app from "./fire";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [emailPassword, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+
+  function handleLogin() {
+    app
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  }
+
+  function handleSignUp() {
+    app
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  }
+
+
+  
+
+
+
+
+
+
   return (
     <div className="App">
       <h1>My app</h1>
---
+++ src/fire.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSAogfaha_I1rqENvdDS8VgGYctU87r1I",
+  authDomain: "login-22c52.firebaseapp.com",
+  projectId: "login-22c52",
+  storageBucket: "login-22c52.appspot.com",
+  messagingSenderId: "826234056573",
+  appId: "1:826234056573:web:07767c0b1549d1b9ffe614",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default fire;
