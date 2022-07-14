505d5deda09a6e8f1e1afa85fde6769950bb4768
Josh <joshmiranda.exe@gmail.com>
2022-07-14 17:31:52 -0400

    signin,signup,login rework

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+};
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,19 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app'
+import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import { getFirestore } from "@firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyDSAogfaha_I1rqENvdDS8VgGYctU87r1I",
   authDomain: "login-22c52.firebaseapp.com",
   projectId: "login-22c52",
   storageBucket: "login-22c52.appspot.com",
   messagingSenderId: "826234056573",
-  appId: "1:826234056573:web:07767c0b1549d1b9ffe614"
-};
+  appId: "1:826234056573:web:07767c0b1549d1b9ffe614",
+});
 
-// Initialize Firebase
-const fire = firebase.initializeApp(firebaseConfig);
-export const db = getFirestore(fire)
-
-export default fire;
-
-
+export const auth = app.auth();
+export default app;
