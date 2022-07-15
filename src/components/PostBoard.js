09746c456b7e5e63ac97e745f077cebb443a8bab
Josh <joshmiranda.exe@gmail.com>
2022-07-15 16:20:02 -0400

    post board

--- src/components/PostBoard.js
+++ src/components/PostBoard.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase";
+import "./Task.css";
 
 export default function PostBoard() {
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
   return (
-    <div>Home</div>
-  )
+    <div className="postBoard">
+      {postList.map((post) => {
+        return (
+        <div className="post">
+          <div className="postHeader">
+            <div className="title">
+              <h1>
+                {post.title}
+              </h1>
+
+            </div>
+
+          </div>
+          <div className="postTextContainer">
+            {post.postText}
+          </div>
+          <h3>@{post.author.name}</h3>
+
+          
+          
+          
+          
+          </div>)
+      })}
+    </div>
+  );
 }
